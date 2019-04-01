4b6dc69f35d1f11de0206b8733a43a84eb5625e5
mattia <biggeb4@gmail.com>
2019-04-01 19:34:31 +0200

    working on menu, added buttons

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public void Load1v1Level()
+    {
+        Debug.Log("sasso");
+        // Load the level named "HighScore".
+        SceneManager.LoadScene("TestLevel");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
